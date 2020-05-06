7471c49f72a8d1007a69efddacff33b045655be0
user <user@DESKTOP-JCSH2GO>
2020-05-06 12:21:20 +0900

    DB조회 시험 과정 1

---
+++ src/lecture1/jdbc1/Book.java
@@ -0,0 +1,47 @@
+package lecture1.jdbc1;
+
+public class Book {
+	int id;
+	String title;
+	String author;
+	int categoryId;
+	String price;
+	String publisher;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+}
